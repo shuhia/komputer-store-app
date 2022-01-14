a952334cca768211724f577abea29ab4b3c247fd
Alex On <shuhia.on@gmail.com>
2022-01-14 13:59:19 +0100

    add api for laptop + display for laptops

--- index.js
+++ index.js
@@ -77,3 +77,79 @@ function User() {
 const balance = document.getElementById("balance");
 
 const user = new User();
+
+const exampleLaptop = {
+  id: 1,
+  title: "Classic Notebook",
+  description: "A little old, but turns on.",
+  specs: [
+    "Has a screen",
+    "Keyboard works, mostly",
+    "32MB Ram (Not upgradable)",
+    "6GB Hard Disk",
+    "Comes with Floppy Disk Reader (Free) - Requires cable",
+    "Good exercise to carry",
+  ],
+  price: 200,
+  stock: 1,
+  active: true,
+  image: "assets/images/1.png",
+};
+
+async function fetchLaptops() {
+  // Fetch laptops from Noroffs API
+  const laptops = await fetch(
+    "https://noroff-komputer-store-api.herokuapp.com/computers"
+  )
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(laptops);
+  return laptops;
+}
+
+fetchLaptops().then((laptops = []) => {
+  // update view
+  // Display laptops
+  const laptopsView = document.querySelector(".laptops");
+  const selectElement = laptopsView.querySelector("select");
+  const featureElement = laptopsView.querySelector("ul");
+  selectElement.innerHTML = laptops.map(
+    (laptop) => `<option value=${laptop.id}>${laptop.title}</option>`
+  );
+
+  function displayFeatures(laptop) {
+    const features = laptop.specs.map((spec) => `<li>${spec}</li>`).join("");
+    featureElement.innerHTML = features;
+  }
+  // display selected laptop
+  if (laptops) displayFeatures(laptops[0]);
+
+  const handleChange = (e) => {
+    const id = e.target.value;
+    const selectedLaptop = laptops.find((laptop) => laptop.id == id);
+    if (selectedLaptop) displayFeatures(selectedLaptop);
+    console.log(laptops);
+  };
+
+  selectElement.addEventListener("change", handleChange);
+
+  // Display selectedLaptop
+});
+
+// Renders a card with select and display features of the selected laptop
+function LaptopsView(element, laptops) {
+  this.selectedLaptop = {};
+  this.laptops = laptops;
+  // Updates the DOM with new HTML code
+  this.render = function () {};
+}
+
+function LaptopView(element, selectedLaptop) {
+  this.view = ``;
+  this.render = function () {
+    element.innerHTML = view;
+  };
+}
