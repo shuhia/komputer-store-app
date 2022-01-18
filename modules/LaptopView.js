fb2e32d175ef415747771d4afbc9f9893ae8ce9f
Alex On <shuhia.on@gmail.com>
2022-01-18 15:18:56 +0100

    fix some HTML validation issues

--- modules/LaptopView.js
+++ modules/LaptopView.js
@@ -22,6 +22,7 @@ export default function LaptopView(element, props) {
     title.innerText = laptop.title;
     description.innerText = laptop.description;
     image.src = baseUrl + "/" + laptop.image;
+    image.alt = laptop.description;
     price.innerText = currencyFormatter.format(laptop.price);
     buy.onclick = () => {
       handleBuy(laptop);
